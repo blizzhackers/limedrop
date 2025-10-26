65f21092f9a235491f87feb259be66bf3d301eff
theBGuy <60308670+theBGuy@users.noreply.github.com>
2025-10-26 13:11:39 -0400

    More support for older files, handle parsing errors, and attempt itemType from description
    - Refactored the v1ItemInfo building to it's own method
    - Added minimal support for determining itemType based on the raw description, only English support right now
    - Handle errors if itemInfo fails to parse and fallback to v1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -93,6 +93,248 @@ export function deepEqual(a: unknown, b: unknown): boolean {
   return true;
 }
 
+function buildV1ItemInfo(itemid: string, desc: string) {
+  const [
+    gid,
+    classid,
+    loc,
+    x,
+    y,
+    ethFlag,
+    rwFlag,
+    itemType,
+    itemQuality,
+    itemClass,
+    sockets,
+    gfx,
+    color,
+  ] = itemid.split(":");
+
+  const codeToQuality = {
+    [sdk.colors.White]: sdk.items.quality.Normal,
+    [sdk.colors.Blue]: sdk.items.quality.Magic,
+    [sdk.colors.NeonGreen]: sdk.items.quality.Set,
+    [sdk.colors.Yellow]: sdk.items.quality.Rare,
+    [sdk.colors.LightGold]: sdk.items.quality.Crafted,
+    [sdk.colors.DarkGold]: sdk.items.quality.Unique,
+    [sdk.colors.Orange]: sdk.items.quality.Normal,
+    [sdk.colors.Gray]: sdk.items.quality.Normal,
+  };
+
+  return {
+    version: 1 as const,
+    gid,
+    id: Number(classid),
+    loc,
+    x,
+    y,
+    ethereal: ethFlag === "1" || ethFlag === "eth",
+    runeword: rwFlag === "1",
+    quality: (() => {
+      if (itemQuality) {
+        return Number(itemQuality);
+      }
+      const code = desc.slice(0, 3);
+      if (code === sdk.colors.Gray && desc.includes("Superior")) {
+        return sdk.items.quality.Superior;
+      }
+      const quality = codeToQuality[code];
+      return quality ?? -1;
+    })(),
+    itemType: (() => {
+      if (itemType) {
+        return Number(itemType);
+      }
+
+      // simplest first
+      if (desc.includes("Jewel")) {
+        return sdk.items.type.Jewel;
+      }
+      if (desc.includes("Ring")) {
+        return sdk.items.type.Ring;
+      }
+      if (desc.includes("Rune")) {
+        return sdk.items.type.Rune;
+      }
+      if (desc.includes("Amulet")) {
+        return sdk.items.type.Amulet;
+      }
+      if (desc.includes("Small Charm")) {
+        return sdk.items.type.SmallCharm;
+      }
+      if (desc.includes("Large Charm")) {
+        return sdk.items.type.LargeCharm;
+      }
+      if (desc.includes("Grand Charm")) {
+        return sdk.items.type.GrandCharm;
+      }
+
+      if (desc.includes("Bow")) {
+        if (desc.includes("Amazon Only")) {
+          return sdk.items.type.AmazonBow;
+        }
+        return sdk.items.type.Bow;
+      }
+
+      if (desc.includes("Crossbow")) {
+        return sdk.items.type.Crossbow;
+      }
+
+      if (desc.includes("Javelin")) {
+        if (desc.includes("Amazon Only")) {
+          return sdk.items.type.AmazonJavelin;
+        }
+        return sdk.items.type.Javelin;
+      }
+
+      if (desc.includes("Spear")) {
+        if (desc.includes("Amazon Only")) {
+          return sdk.items.type.AmazonSpear;
+        }
+        return sdk.items.type.Spear;
+      }
+
+      if (
+        desc.includes("Armor") ||
+        desc.includes("Mail") ||
+        desc.includes("Plate") ||
+        desc.includes("Cuirass") ||
+        desc.includes("Scarab Husk")
+      ) {
+        return sdk.items.type.Armor;
+      }
+
+      if (desc.includes("Claw")) {
+        return sdk.items.type.AssassinClaw;
+      }
+
+      if (desc.includes("Chance to Block")) {
+        if (desc.includes("Paladin Only")) {
+          return sdk.items.type.AuricShields;
+        }
+        if (desc.includes("Necromancer Only")) {
+          return sdk.items.type.VoodooHeads;
+        }
+        return sdk.items.type.Shield;
+      }
+
+      if (desc.includes("Axe")) {
+        if (desc.includes("Quantity")) {
+          return sdk.items.type.ThrowingAxe;
+        }
+        return sdk.items.type.Axe;
+      }
+
+      if (
+        desc.includes("Belt") ||
+        desc.includes("Sash") ||
+        desc.includes("Coil")
+      ) {
+        return sdk.items.type.Belt;
+      }
+
+      if (desc.includes("Boots") || desc.includes("Greaves")) {
+        return sdk.items.type.Boots;
+      }
+
+      if (
+        desc.includes("Circlet") ||
+        desc.includes("Tiara") ||
+        desc.includes("Diadem")
+      ) {
+        return sdk.items.type.Circlet;
+      }
+
+      if (desc.includes("Mace")) {
+        if (desc.includes("Hammer") || desc.includes("Maul")) {
+          return sdk.items.type.Hammer;
+        }
+
+        if (desc.includes("Scepter")) {
+          return sdk.items.type.Scepter;
+        }
+        return sdk.items.type.Mace;
+      }
+
+      if (
+        desc.includes("Helm") ||
+        desc.includes("Mask") ||
+        desc.includes("Crown") ||
+        desc.includes("Casque") ||
+        desc.includes("Cap")
+      ) {
+        return sdk.items.type.Helm;
+      }
+
+      if (desc.includes("Dagger")) {
+        if (desc.includes("Quantity")) {
+          return sdk.items.type.ThrowingKnife;
+        }
+        return sdk.items.type.Knife;
+      }
+
+      if (desc.includes("Staff")) {
+        if (desc.includes("Sorceress Only")) {
+          return sdk.items.type.Orb;
+        }
+        if (desc.includes("Necromancer Only") || desc.includes("Wand")) {
+          return sdk.items.type.Wand;
+        }
+        return sdk.items.type.Staff;
+      }
+
+      if (
+        desc.includes("Druid Only") &&
+        (desc.includes("Antlers") ||
+          desc.includes("Helm") ||
+          desc.includes("Head") ||
+          desc.includes("Mask"))
+      ) {
+        return sdk.items.type.Pelt;
+      }
+
+      if (desc.includes("Polearm")) {
+        return sdk.items.type.Polearm;
+      }
+
+      if (
+        desc.includes("Barbarian Only") &&
+        (desc.includes("Helm") ||
+          desc.includes("Crown") ||
+          desc.includes("Guard"))
+      ) {
+        return sdk.items.type.PrimalHelm;
+      }
+
+      if (desc.includes("Sword")) {
+        return sdk.items.type.Sword;
+      }
+
+      if (
+        desc.includes("Gloves") ||
+        desc.includes("Gauntlets") ||
+        desc.includes("Bracers") ||
+        desc.includes("Vambraces") ||
+        desc.includes("Mitts")
+      ) {
+        return sdk.items.type.Gloves;
+      }
+
+      return 0; // Unknown
+    })(),
+    itemClass: Number(itemClass),
+    sockets: (() => {
+      if (sockets) {
+        return Number(sockets);
+      }
+      const match = desc.match(/Socketed \((\d+)\)/);
+      return match ? Number(match[1]) : 0;
+    })(),
+    gfx: Number(gfx),
+    color: Number(color),
+  };
+}
+
 export function extractItemInfo(itemid: string, desc: string) {
   /**
     // From Mulelogger.logItem
@@ -134,80 +376,27 @@ export function extractItemInfo(itemid: string, desc: string) {
    */
   const [gid, classid, loc, x, y, itemInfo] = itemid.split(":");
 
-  const codeToQuality = {
-    [sdk.colors.White]: sdk.items.quality.Normal,
-    [sdk.colors.Blue]: sdk.items.quality.Magic,
-    [sdk.colors.NeonGreen]: sdk.items.quality.Set,
-    [sdk.colors.Yellow]: sdk.items.quality.Rare,
-    [sdk.colors.LightGold]: sdk.items.quality.Crafted,
-    [sdk.colors.DarkGold]: sdk.items.quality.Unique,
-    [sdk.colors.Orange]: sdk.items.quality.Normal,
-    [sdk.colors.Gray]: sdk.items.quality.Normal,
-  };
-
   // handle old logs without itemInfo
   if (!itemInfo || itemInfo.length === 1 || itemInfo === "eth") {
-    const [
-      _gid,
-      _classid,
-      _loc,
-      _x,
-      _y,
-      ethFlag,
-      rwFlag,
-      itemType,
-      itemQuality,
-      itemClass,
-      sockets,
-      gfx,
-      color,
-    ] = itemid.split(":");
+    return buildV1ItemInfo(itemid, desc);
+  }
+
+  try {
+    const itemDump = JSON.parse(atob(itemInfo)) as ItemInfo;
 
     return {
-      version: 1 as const,
+      version: 2 as const,
       gid,
-      id: Number(classid),
+      classid: Number(classid),
       loc,
       x,
       y,
-      ethereal: ethFlag === "1" || ethFlag === "eth",
-      runeword: rwFlag === "1",
-      quality: (() => {
-        if (itemQuality) {
-          return Number(itemQuality);
-        }
-        const code = desc.slice(0, 3);
-        if (code === sdk.colors.Gray && desc.includes("Superior")) {
-          return sdk.items.quality.Superior;
-        }
-        const quality = codeToQuality[code];
-        return quality ?? -1;
-      })(),
-      itemType: Number(itemType),
-      itemClass: Number(itemClass),
-      sockets: (() => {
-        if (sockets) {
-          return Number(sockets);
-        }
-        const match = desc.match(/Socketed \((\d+)\)/);
-        return match ? Number(match[1]) : 0;
-      })(),
-      gfx: Number(gfx),
-      color: Number(color),
+      ...itemDump,
     };
+  } catch (e) {
+    console.error("Failed to parse item info:", e);
+    return buildV1ItemInfo(itemid, desc);
   }
-
-  const itemDump = JSON.parse(atob(itemInfo)) as ItemInfo;
-
-  return {
-    version: 2 as const,
-    gid,
-    classid: Number(classid),
-    loc,
-    x,
-    y,
-    ...itemDump,
-  };
 }
 
 // Natural sort helper for account names
